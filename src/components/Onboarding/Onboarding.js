7c1db1e09fb21357322a2a6edb1c6b42c4fb1d70
Monisha Tarkar <monishatarkar1625@gmail.com>
2022-03-14 01:45:58 +0530

    cards

--- src/components/Onboarding/Onboarding.js
+++ src/components/Onboarding/Onboarding.js
@@ -2,32 +2,47 @@ import React,{useEffect, useState} from 'react';
 import StepProgress from '../StepProgress/StepProgress';
 import Input from '../Elements/Input/Input';
 import Button from '../Elements/Button/Button';
+import Card from '../Elements/Card/Card';
 import styles from './onboarding.module.scss';
+import check from '../../assests/icons/check-lg.svg';
+
 
 function Onboarding() {
   const tabHeadings = [
     {
-      tab: 0,
+      tab: 1,
       main: "Welcome! First things First...",
       sub: "You can always change them later."
     },
     {
-      tab: 1,
+      tab: 2,
       main: "Let's set up a home for all your work",
       sub: "You can always create another workspace later."
     },
     {
-      tab: 2,
+      tab: 3,
       main: "How are you planning to use Eden?",
       sub: "We'll streamline your setup experience accordingly."
     },
     {
-      tab: 3,
+      tab: 4,
       main: "",
       sub: ""
     }
-  ]
-  const [tabNumber, setTabNumber] = useState(0);
+  ];
+  const usageCards = [
+    {
+      id: 1,
+      title: 'For myself',
+      text: 'Write better.Think more clearly.Stay organized.'
+    },
+    {
+      id: 2,
+      title: 'With my team',
+      text: 'Wikis, docs, tasks & projects, all in one place.'
+    }
+  ];
+  const [tabNumber, setTabNumber] = useState(1);
   const [user, setUser] = useState({
     firstName: '',
     displayName: '',
@@ -52,56 +67,62 @@ function Onboarding() {
   },[formState])
 
   function handleClick(){
-    setFormState({...user,...workSpace,...usage})
+    setFormState({...user,...workSpace,...usage});
+    // if (tabNumber === 3) setTabNumber((tab) => tab + 1);
   }
   function handleFormSubmit(e){
     e.preventDefault();
-    console.log("formState", formState);
-    if (tabNumber === '3') return;
+    // console.log("formState", formState);
+    if (tabNumber === '4') return;
     setTabNumber((tab) => tab + 1);
+  }
+
+  function handleCardSelect(){
+    console.log("usage",usage)
+
   }
 
   return (
     <div className={styles.Onboarding}>
 
-      <StepProgress tab={tabNumber}/>
+      <StepProgress tab={tabNumber} numberSteps={4}/>
 
       <div className={styles.tabHeader}>
-        <span className={styles.tabHeader__main}>{tabHeadings[tabNumber].main}</span>
-        <span className={styles.tabHeader__sub}>{tabHeadings[tabNumber].sub}</span>
+        <span className={styles.tabHeader__main}>{tabHeadings[tabNumber-1].main}</span>
+        <span className={styles.tabHeader__sub}>{tabHeadings[tabNumber-1].sub}</span>
       </div>
 
       <div className={styles.form_section}>
         
-          { tabNumber === 0 &&
+          { tabNumber === 1 &&
           <>
-          <form onSubmit={handleFormSubmit}>
-            <Input 
-              id="text"
-              label="First Name"
-              placeholder='Steve Jobs'
-              value={user.firstName}
-              onChange={(value) => setUser({...user, 'firstName': value })}
-              minLength="2"
-              maxLength="25"
-            />
-            <Input
-              id="text"
-              label="Display Name"
-              placeholder='Steve'
-              value={user.displayName}
-              onChange={(value) => setUser({...user,'displayName': value})}
-              minLength="2"
-              maxLength="25"
-            />
-            <Button text="Create Workspace" handleClick={handleClick} /> 
-          </form>
+            <form onSubmit={handleFormSubmit} style={{width:'20rem'}}>
+              <Input 
+                id="text"
+                label="First Name"
+                placeholder='Steve Jobs'
+                value={user.firstName}
+                onChange={(value) => setUser({...user, 'firstName': value })}
+                minLength="2"
+                maxLength="25"
+              />
+              <Input
+                id="text"
+                label="Display Name"
+                placeholder='Steve'
+                value={user.displayName}
+                onChange={(value) => setUser({...user,'displayName': value})}
+                minLength="2"
+                maxLength="25"
+              />
+              <Button text="Create Workspace" handleClick={handleClick} /> 
+            </form>
           </>
         
           }
-          {tabNumber === 1 &&
-            <>
-          <form onSubmit={handleFormSubmit}>
+          {tabNumber === 2 &&
+          <>
+          <form onSubmit={handleFormSubmit} style={{ width: '20rem' }}>
               <Input
                 id="text"
                 label="Workspace Name"
@@ -121,17 +142,30 @@ function Onboarding() {
                 maxLength="25"
               />
             <Button text="Create Workspace" handleClick={handleClick} />
-            </form>
-            </>
-          }
-          {tabNumber === 2 &&
-          <>
-          <form onSubmit={handleFormSubmit}>
-            <span>Random Card</span>
-            <Button text="Create Workspace" handleClick={handleClick} />
           </form>
-          </> 
-          
+          </>
+          }
+          {tabNumber === 3 &&
+          <div className={styles.tabThree}>
+            <div className={styles.cardsContainer}>
+              {usageCards.map((card) => {
+                return <Card key={card.id} card={card} setUsage={setUsage} handleCardSelect={handleCardSelect} />
+              })}
+            </div>
+            <Button text="Create Workspace" handleClick={handleClick} />
+          </div>
+          }
+          { tabNumber === 4 &&
+          <>
+              <div className={styles.tabFour}>
+                <div className={styles.tabFour__img}>
+                  <img src={check} alt="check" />
+                </div>
+                <h1>Congratulations, {formState.displayName}!</h1>
+                <p>You have completed onboarding, you can start using the Eden!</p>
+              </div>
+              <Button text="Launch Eden" handleClick={handleClick} />
+          </>
           }
         
       </div>
--- src/components/StepProgress/StepProgress.js
+++ src/components/StepProgress/StepProgress.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from 'react';
+import styles from './stepProgress.module.scss';
+// import classes from './stepProgress.module.scss';
+function StepProgress({ tab, numberSteps }) {
+  let numSteps;
+  // numSteps = arrayofDigits(4);
+  if (numberSteps === null || undefined) {
+    numSteps = arrayofDigits(numberSteps);
+  } else {
+    numSteps = arrayofDigits(4);
+  }
 
-function StepProgress() {
+  // Convert Number to array
+  function arrayofDigits(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
   return (
-    <div>StepProgress</div>
-  )
-}
+    <div className={styles['stepper']} >
+      <ul className={styles['steps']}>
+        {numSteps.map((item) => {
+          return (
+            <li
+              className={
+                (tab === item
+                  ? `${styles['step']} ${styles['step__incomplete']} ${styles['step__active']}`
+                  : tab < item
+                    ? ` ${styles['step']} ${styles['step__incomplete']} ${styles['step__inactive']}`
+                    : ` ${styles['step']} ${styles['step__complete']} ${styles['step__inactive']}`)
+              }
+              key={item}
+            >
+              <span className={styles['step__icon']}>{item}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+// let progressWidth;
+//   switch (tab) {
+//     case 1:
+//       progressWidth = 20;
+//       break;
+//     case 2:
+//       progressWidth = 50;
+//       break;
+//     case 3:
+//       progressWidth = 80;
+//       break;
+//     case 4:
+//       progressWidth = 100;
+//       break;
+
+//     default:
+//       progressWidth = 20;
+//       break;
+//   }
+//   const NUMBER_OF_STEPS = [0,1,2,3];
+
+//     const myStyle = {
+//       width: `${progressWidth}%`
+//     };
+//     const progressBar = NUMBER_OF_STEPS.map((id) => {
+//       if (id <= tab) {
+//         return (
+//           <div
+//             key={id}
+//             // onClick={() => props.changeStep(id)}
+//             className={` ${classes['progress-step']}  ${classes['progress-step-active']}`}
+//           />
+//         );
+//       } else { //onClick={() => props.changeStep(id)}
+//         return <div key={id}  className={classes['progress-step']} />;
+//       }
+//     });
+
+//     return (
+//       <React.Fragment>
+//         <div className={classes.progressbar}>
+//           <div className={classes.progress} style={myStyle} />
+//           {progressBar}
+//         </div>
+//       </React.Fragment>
+//     );
+//   };
+
 
 export default StepProgress;
